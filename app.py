5c697c38b6230c2b814a0397fe0138c02a9d89fc
Oscar Caicedo <oscarccd17@gmail.com>
2022-08-11 23:03:14 -0500

    creating Managers class

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-# Create a class with Managers
-# Create a class with Employees
+# Create a class with Managers (attrs: first, last, email, loc)
+# Create an Employees subclass
+# Create a method that will display Manager first, last, email, and location
+# Create a method that will display Employees first, last, email, and location
+
+
+class Managers:
+    def __init__(self, first, last, email, location):
+        self.first = first
+        self.last = last
+        self.email = email
