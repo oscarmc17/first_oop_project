930328d2c50726dcbb449d3fd90d8d019b7e55c9
Oscar Caicedo <oscarccd17@gmail.com>
2022-08-15 22:57:00 -0500

    starting to implement abstracmethods

--- app.py
+++ app.py
@@ -7,26 +7,33 @@
 
 
 
-class Managers:
-    def __init__(self, first, last, email, location):
-        self.first = first
-        self.last = last
-        self.email = email
-        self.location = location
+# class Managers:
+#     def __init__(self, first, last, email, location):
+#         self.first = first
+#         self.last = last
+#         self.email = email
+#         self.location = location
 
 
-class Employees(Managers):
-    def __init__(self, first, last, email, location, age):
-        super().__init__(first, last, email, location)
-        self.age = age
+# class Employees(Managers):
+#     def __init__(self, first, last, email, location, age):
+#         super().__init__(first, last, email, location)
+#         self.age = age
 
 
-manager1 = ("Oscar", "Caicedo", "oscarcaicedo@epsilon.com", "Chicago, IL")
-manager2 = ("Jose", "Armas", "JoseArmas@epsilon.com", "Chicago, IL")
+# manager1 = ("Oscar", "Caicedo", "oscarcaicedo@epsilon.com", "Chicago, IL")
+# manager2 = ("Jose", "Armas", "JoseArmas@epsilon.com", "Chicago, IL")
 
-emp1 = ("Carlos", "Minero", "Cminero1@yahoo.com", 28)
+# emp1 = ("Carlos", "Minero", "Cminero1@yahoo.com", 28)
 
 
-print(manager1)
-print(manager2)
-print(emp1)
+# print(manager1)
+# print(manager2)
+# print(emp1)
+
+from abc import ABC, abstractmethod
+
+class Employee(ABC):
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
