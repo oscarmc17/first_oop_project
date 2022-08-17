2ab73e250fa4745ba7c693e71e4748e3b84f6b06
Oscar Caicedo <oscarccd17@gmail.com>
2022-08-16 22:30:07 -0500

    adding my decorators. Ex: @abstractmethods + import

--- hello.py
+++ hello.py
@@ -1,10 +1,18 @@
-class Shape:
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
     def area(self):
         pass
 
-    def perimeter(self): 
+    @abstractmethod
+    def perimeter(self):
         pass
+
 
 class Square(Shape):
     def __init__(self, side):
         self.__side = side
+
+
+shape = Shape()
