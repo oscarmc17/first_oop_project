8df9f157315a3d3ef450519ebf771f22a2a43b41
Oscar Caicedo <oscarccd17@gmail.com>
2022-08-20 13:39:17 -0500

    same method but different implementation

--- polymorphism.py
+++ polymorphism.py
@@ -1,3 +1,23 @@
 # Poly-morphism
 # Poly = many
 # Morphism = form  "Many Forms"
+
+class French:
+    def say_hello(self):
+        print('Bonjour')
+
+
+class Chinese:
+    def say_hello(self):
+        print('Nihao')
+
+
+def intro(lang):
+    lang.say_hello()
+
+
+sarthak = French()
+john = Chinese()
+
+intro(sarthak)
+intro(john)
