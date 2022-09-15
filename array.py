6960bfbddab16618ba30c3846eff6f054fee8d72
Oscar Caicedo <oscarccd17@gmail.com>
2022-09-15 12:55:10 -0500

    sorted list algorithm example 2

--- array.py
+++ array.py
@@ -1,6 +1,4 @@
-from turtle import left, right
-
-
+# Merge Sort example. O (n log n) TC
 def merge_sort(arr):
     if len(arr) > 1:
         left_arr = arr[:len(arr)//2]
@@ -11,32 +9,29 @@ def merge_sort(arr):
         merge_sort(right_arr)
 
         # merge
-        i = 0 # keeps track of leftmost element in left array 
-        j = 0 # keeps track of leftmost element in right array
-        k = 0 # merged array index
+        i = 0  # keeps track of leftmost element in left array
+        j = 0  # keeps track of leftmost element in right array
+        k = 0  # merged array index
 
         while i < len(left_arr) and j < len(right_arr):
             if left_arr[i] < right_arr[j]:
                 arr[k] = left_arr[i]
                 i += 1
-            else: 
+            else:
                 arr[k] = right_arr[j]
                 j += 1
             k += 1
-        
-        
+
         while i < len(left_arr):
             arr[k] = left_arr[i]
             i += 1
             k += 1
-        
+
         while j < len(right_arr):
             arr[k] = right_arr[j]
             j += 1
             k += 1
 
-
 arr_test = [2, 3, 5, 1, 7, 4, 4, 4, 2, 6, 0]
 merge_sort(arr_test)
-
 print(arr_test)
